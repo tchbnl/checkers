0c7fbf119b6d6901206b7c6e236c569d2a5adfa1
Nathan Paton <86271004+tchbnl@users.noreply.github.com>
2022-07-27 17:51:23 -0400

    v0.1

--- minicheck.sh
+++ minicheck.sh
@@ -4,13 +4,13 @@ unset GOTEM
 fi
 if [[ $1 = "" ]]; then
 echo "You must specify the error message."
-exit
+return
 fi
 WP_CLI="php -d memory_limit=128M -d disable_functions= $(which wp)"
 URL="$($WP_CLI option get siteurl --skip-plugins --skip-themes)"
 if [[ $(curl -A "checkers" $URL 2>&1 | grep -i "$1") = "" ]]; then
 echo "I don't see that error message on the site."
-exit
+return
 fi
 PLUGINS="$($WP_CLI plugin list --field=name --status=active --skio-plugins --skip-themes 2>/dev/null)"
 echo -e "Here's our suspects: "$PLUGINS"\n"
@@ -20,7 +20,7 @@ echo -e "Let's see if it's ${PLUGIN}..."
 if [[ $(curl -A "checkers" $URL 2>&1 | grep -i "$1") = "" ]]; then
 echo -e "Found it\u21 $PLUGIN is the imposter."
 GOTEM="true"
-exit; else
+return; else
 echo -e "It wasn't ${PLUGIN}. Back to the drawing board.\n"
 wpActivatePlugin="$($WP_CLI plugin activate $PLUGIN)"
 fi
