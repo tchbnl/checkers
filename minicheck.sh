3a819ca87dbcab94490cdefb40ab9bdbb4aa6286
Nathan Paton <86271004+tchbnl@users.noreply.github.com>
2022-07-28 14:06:28 -0400

    v0.2

--- minicheck.sh
+++ minicheck.sh
@@ -7,13 +7,13 @@ TEXT_BLD="\e[1m"
 TEXT_RST="\e[0m"
 if [[ $1 = "" ]]; then
 echo "You need to specify an error message. Use: checkers \"Your error message\""
-exit
+return
 fi
 WP_CLI="php -d memory_limit=128M -d disable_functions= $(which wp)"
 URL="$($WP_CLI option get siteurl --skip-plugins --skip-themes 2>/dev/null)"
 if [[ $(curl -s -A "checkers" $URL 2>&1 | grep -i "$1") = "" ]]; then
 echo "I don't see that error message on the site."
-exit
+return
 fi
 PLUGINS="$($WP_CLI plugin list --field=name --status=active --skip-plugins --skip-themes 2>/dev/null)"
 echo -e "${TEXT_BLD}The usual suspects:${TEXT_RST}\n$($WP_CLI plugin list --field=title --status=active --skip-plugins --skip-themes 2>/dev/null | awk '{print "* "$0}')\n"
@@ -24,7 +24,7 @@ echo -e "${TEXT_BLD}Let's check $PLUGIN_NAME${TEXT_RST}:"
 if [[ $(curl -s -A "checkers" $URL 2>&1 | grep -i "$1") = "" ]]; then
 echo -e "👮‍♂️ Gotcha'"'!'" $PLUGIN_NAME is the imposter."
 GOTEM="true"
-exit; else
+return; else
 echo -e "😔 Nope. It wasn't $PLUGIN_NAME.\n"
 wpActivatePlugin="$($WP_CLI plugin activate $PLUGIN --skip-plugins --skip-themes 2>/dev/null)"
 fi
