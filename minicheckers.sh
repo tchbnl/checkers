095af95df90df14b5766ca963e4153be46f16b2a
Nathan Paton <86271004+tchbnl@users.noreply.github.com>
2022-08-02 22:11:13 -0400

    v0.3

--- minicheckers.sh
+++ minicheckers.sh
@@ -5,7 +5,7 @@ TEXT_RESET='\e[0m'
 if [[ -n "$1" ]]; then
 ERROR_MSG="$1"; else
 echo "You need to specify an error message. Use: checkers \"Your error message\""
-exit
+return
 fi
 wp_cli() {
 php -d memory_limit=128M -d disable_functions= "$(which wp)" "$@" --skip-plugins --skip-themes 2>/dev/null
@@ -14,7 +14,7 @@ URL="$(wp_cli option get siteurl)"
 USER_AGENT="Mozilla/3.0 (compatible; NetPositive/2.1.1; BeOS)"
 if ! curl -sA "${USER_AGENT}" "${URL}" | grep -iq "${ERROR_MSG}"; then
 echo "I don't see that error message on the site."
-exit
+return
 fi
 PLUGINS="$(wp_cli plugin list --field=name --status=active)"
 echo -e "${TEXT_BOLD}Here's our suspects:${TEXT_RESET}
@@ -26,7 +26,7 @@ echo -e "${TEXT_BOLD}Let's check ${SUSPECT}:${TEXT_RESET}"
 if ! curl -sA "${USER_AGENT}" "${URL}" | grep -iq "${ERROR_MSG}"; then
 echo "👮‍♂️ Gotcha"'!'" ${SUSPECT} is the imposter."
 FOUND='true'
-exit; else
+return; else
 echo -e "😔 Nope, it wasn't ${SUSPECT}.\n"
 wp_cli plugin activate "${PLUGIN}" --quiet
 fi
